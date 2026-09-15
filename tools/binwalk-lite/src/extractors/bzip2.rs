aa8909f162db81829be615fe05dc5a85dbcb7f08
liku <168651712+yjw0827@users.noreply.github.com>
2026-09-15 14:16:37 +0900

    Update bzip2.rs

--- tools/binwalk-lite/src/extractors/bzip2.rs
+++ tools/binwalk-lite/src/extractors/bzip2.rs
@@ -35,7 +35,7 @@ pub fn bzip2_extractor() -> Extractor {
 pub fn bzip2_decompressor(
     file_data: &[u8],
     offset: usize,
-    output_directory: Option<&str>,
+    output_directory: Option<&string>,
 ) -> ExtractionResult {
     // Size of decompression buffer
     const BLOCK_SIZE: usize = 900 * 1024;
